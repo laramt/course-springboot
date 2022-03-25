a3796a87db5f72077eea2da7f32091b57accfb6e
Lara Menck <lara.menckt@gmail.com>
2022-03-25 15:19:16 -0300

    Category

--- src/main/java/com/cursojava/course/config/TesteConfig.java
+++ src/main/java/com/cursojava/course/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursojava.course.entities.Category;
 import com.cursojava.course.entities.Order;
 import com.cursojava.course.entities.User;
 import com.cursojava.course.entities.enums.OrderStatus;
+import com.cursojava.course.repositories.CategoryRepository;
 import com.cursojava.course.repositories.OrderRepository;
 import com.cursojava.course.repositories.UserRepository;
 
@@ -24,9 +26,17 @@ public class TesteConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category category1 = new Category(null, "Electronics");
+		Category category2 = new Category(null, "Books");
+		Category category3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
 		
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User user2 =  new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -35,8 +45,10 @@ public class TesteConfig implements CommandLineRunner {
 		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
 		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 		
+		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+		
 	}
 	
 	
